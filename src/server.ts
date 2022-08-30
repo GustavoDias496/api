1a43a7e9f83506c835ef45c713603727aa6e5657
GustavoDias496 <88061007+GustavoDias496@users.noreply.github.com>
2022-08-30 00:05:09 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const app = express()
 app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
-    res.header('Acesss-Control-Allow-Headers', 'Content-Type')
+    res.header('Acesss-Control-Allow-Headers', '*')
     next()
 })
 
